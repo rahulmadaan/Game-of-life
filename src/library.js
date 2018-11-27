a0189a7beeec73f01a0a62c81a86f31504f9abe9
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-11-27 23:24:54 +0530

    library and libraryTest is beautified

--- src/library.js
+++ src/library.js
@@ -1,31 +1,31 @@
-const repeat = function(symbol,length) {
+const repeat = function(symbol, length) {
   return new Array(length).fill(symbol).join('');
 }
 
-const generateLine = (length)=>repeat("-", length);
+const generateLine = (length) => repeat("-", length);
 
-const createBoard = function(data){
+const createBoard = function(data) {
   let result = [];
   let lengthOfData = data.length;
-  if(lengthOfData === 0 || data[0].length === 0){
+  if (lengthOfData === 0 || data[0].length === 0) {
     return result;
   }
-  result.push( generateLine(lengthOfData*4+1) );
-  for(let index=0; index<lengthOfData; index++) {
+  result.push(generateLine(lengthOfData * 4 + 1));
+  for (let index = 0; index < lengthOfData; index++) {
     result.push(joinWithPipes(data[index]).join(""));
-    result.push( generateLine(lengthOfData*4+1));
-  }  
+    result.push(generateLine(lengthOfData * 4 + 1));
+  }
   return result.join("\n");
 }
 
 const joinWithPipes = function(input) {
   let output = [];
-  if(input[0] !== undefined){
+  if (input[0] !== undefined) {
     output.push('|');
   }
-  for(value of input) {
-    output.push(" " +value + " |");
-  } 
+  for (value of input) {
+    output.push(" " + value + " |");
+  }
   return output;
 }
 
@@ -35,54 +35,54 @@ const generateDeads = function(length) {
 
 const fillBoard = function(length) {
   let board = [];
-  for(let count=0; count<length; count++) {
+  for (let count = 0; count < length; count++) {
     board.push(generateDeads(length));
   }
   return board;
 }
 
-const getNeighbour = function(inputArray,index) {
+const getNeighbour = function(inputArray, index) {
   let neighbours = [];
-  if(index < 0){return neighbours}
-  neighbours.push(inputArray[index-1]);
-  neighbours.push(inputArray[index+1]);
-  return neighbours.filter( (x)=>x );
+  if (index < 0) { return neighbours }
+  neighbours.push(inputArray[index - 1]);
+  neighbours.push(inputArray[index + 1]);
+  return neighbours.filter((x) => x);
 }
 
-const getHorizontalNeighbours = function(inputArray, arrayIndex, subArrayIndex){
+const getHorizontalNeighbours = function(inputArray, arrayIndex, subArrayIndex) {
   return getNeighbour(inputArray[arrayIndex], subArrayIndex);
 }
 
-const getVerticalNeighbours = function(inputArray,arrayIndex,subArrayIndex) {
+const getVerticalNeighbours = function(inputArray, arrayIndex, subArrayIndex) {
   let neighbours = [];
-  if (inputArray.length === 0){return neighbours;}
+  if (inputArray.length === 0) { return neighbours; }
 
-  let topNeighbours = inputArray[arrayIndex-1];
-  let bottomNeighbours = inputArray[arrayIndex+1];
-  if (topNeighbours !== undefined){
+  let topNeighbours = inputArray[arrayIndex - 1];
+  let bottomNeighbours = inputArray[arrayIndex + 1];
+  if (topNeighbours !== undefined) {
     neighbours.push(topNeighbours[subArrayIndex]);
   }
-  if (bottomNeighbours !== undefined){
+  if (bottomNeighbours !== undefined) {
     neighbours.push(bottomNeighbours[subArrayIndex]);
   }
   return neighbours;
 }
 
-const getAllNeighbours = function(inputArray,arrayIndex,subArrayIndex) {
+const getAllNeighbours = function(inputArray, arrayIndex, subArrayIndex) {
   let neighbours = [];
-  if (arrayIndex < 0 || subArrayIndex < 0 || inputArray.length === 0){
+  if (arrayIndex < 0 || subArrayIndex < 0 || inputArray.length === 0) {
     return neighbours;
   }
 
-  neighbours.push(getNeighbour(inputArray[arrayIndex],subArrayIndex));  // horizontal neighbours
-  if(inputArray[arrayIndex-1] !== undefined){
-  neighbours.push(getNeighbour(inputArray[arrayIndex-1],subArrayIndex)); // top diagonal neighbours
+  neighbours.push(getNeighbour(inputArray[arrayIndex], subArrayIndex)); // horizontal neighbours
+  if (inputArray[arrayIndex - 1] !== undefined) {
+    neighbours.push(getNeighbour(inputArray[arrayIndex - 1], subArrayIndex)); // top diagonal neighbours
   }
-  if(inputArray[arrayIndex+1] !== undefined){
-  neighbours.push(getNeighbour(inputArray[arrayIndex+1],subArrayIndex)); // bottom diagonal neighbours
+  if (inputArray[arrayIndex + 1] !== undefined) {
+    neighbours.push(getNeighbour(inputArray[arrayIndex + 1], subArrayIndex)); // bottom diagonal neighbours
   }
-  neighbours.push(getVerticalNeighbours(inputArray, arrayIndex, subArrayIndex) );
-  return neighbours.reduce( (acc, value)=> [...acc, ...value], [] ).sort();  
+  neighbours.push(getVerticalNeighbours(inputArray, arrayIndex, subArrayIndex));
+  return neighbours.reduce((acc, value) => [...acc, ...value], []).sort();
 }
 
 
@@ -97,5 +97,3 @@ module.exports = {
   getVerticalNeighbours,
   getAllNeighbours
 };
-
-
